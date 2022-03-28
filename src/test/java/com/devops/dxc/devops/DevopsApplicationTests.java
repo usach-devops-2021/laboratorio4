fffe902a9bd9de318ac66563b778be88d3c524a7
Wladimir Román <95427422+wromantapia@users.noreply.github.com>
2022-03-28 16:30:21 -0300

    Mejora en identación

--- src/test/java/com/devops/dxc/devops/DevopsApplicationTests.java
+++ src/test/java/com/devops/dxc/devops/DevopsApplicationTests.java
@@ -82,58 +82,58 @@ class DevopsApplicationTests {
 
 	@Test
 	public void seleniumTest() throws Exception{
-	  System.out.print("**********************Ejecutando Test de Selenium***********************");
-	  JavascriptExecutor js;
-	  WebDriver driver;
-	  String resultado;
-	  Integer timout = 4;
+		System.out.print("**********************Ejecutando Test de Selenium***********************");
+		JavascriptExecutor js;
+		WebDriver driver;
+		String resultado;
+		Integer timout = 4;
 
-	  String OS = System.getProperty("os.name").toLowerCase();
-	  System.out.println("OS: " + OS);
-	  if(OS.contains("win")){
+		String OS = System.getProperty("os.name").toLowerCase();
+		System.out.println("OS: " + OS);
+		if(OS.contains("win")){
 		System.out.println("Es Windows");
 		System.setProperty("webdriver.chrome.driver","src/driver/win/chromedriver.exe"); // Windows
-	  }else{
+		}else{
 		System.out.println("No es Windows");
 		System.setProperty("webdriver.chrome.driver","src/driver/linx/chromedriver"); // Linux
-	  }
+		}
 
-	  // Chrome Driver Options (Utiliza el navegador sin interfaz)
-	  ChromeOptions options = new ChromeOptions();
-	  options.addArguments("--headless");
-	  options.addArguments("--no-sandbox");
-	  options.addArguments("--disable-dev-shm-usage");
-	  driver = new ChromeDriver(options);
-	  Integer sleepTimeMill = 30000; // 30 segundos
+		// Chrome Driver Options (Utiliza el navegador sin interfaz)
+		ChromeOptions options = new ChromeOptions();
+		options.addArguments("--headless");
+		options.addArguments("--no-sandbox");
+		options.addArguments("--disable-dev-shm-usage");
+		driver = new ChromeDriver(options);
+		Integer sleepTimeMill = 30000; // 30 segundos
 
-	  js = (JavascriptExecutor) driver;
-	  System.out.print("Ejecutando Test de Selenium");
-	  driver.get("http://localhost:3000/");
-	  driver.manage().window().setSize(new Dimension(1276, 693));
-	  driver.findElement(By.id("input_sueldo")).click();
-	  driver.findElement(By.id("input_sueldo")).sendKeys("2000000");
-	  driver.findElement(By.id("input_ahorro")).click();
-	  driver.findElement(By.id("input_ahorro")).sendKeys("10000000");
+		js = (JavascriptExecutor) driver;
+		System.out.print("Ejecutando Test de Selenium");
+		driver.get("http://localhost:3000/");
+		driver.manage().window().setSize(new Dimension(1276, 693));
+		driver.findElement(By.id("input_sueldo")).click();
+		driver.findElement(By.id("input_sueldo")).sendKeys("2000000");
+		driver.findElement(By.id("input_ahorro")).click();
+		driver.findElement(By.id("input_ahorro")).sendKeys("10000000");
 
-	  // Test DXC
-	  driver.findElement(By.id("btn_dxc")).click();
-	  TimeUnit.MILLISECONDS.sleep(sleepTimeMill);
-	  resultado = driver.findElement(By.id("resultado")).getText();
-	  assertEquals("DXC: 1000000", resultado);
+		// Test DXC
+		driver.findElement(By.id("btn_dxc")).click();
+		TimeUnit.MILLISECONDS.sleep(sleepTimeMill);
+		resultado = driver.findElement(By.id("resultado")).getText();
+		assertEquals("DXC: 1000000", resultado);
 
-	  // Test Saldo
-	  driver.findElement(By.id("btn_saldo")).click();
-	  TimeUnit.MILLISECONDS.sleep(sleepTimeMill);
-	  resultado = driver.findElement(By.id("resultado")).getText();
-	  assertEquals("Saldo: 9000000", resultado);
+		// Test Saldo
+		driver.findElement(By.id("btn_saldo")).click();
+		TimeUnit.MILLISECONDS.sleep(sleepTimeMill);
+		resultado = driver.findElement(By.id("resultado")).getText();
+		assertEquals("Saldo: 9000000", resultado);
 
-      // Test Impuesto
-      driver.findElement(By.id("btn_impuesto")).click();
-      TimeUnit.MILLISECONDS.sleep(sleepTimeMill);
-      resultado = driver.findElement(By.id("resultado")).getText();
-      assertEquals("Impuesto: 80000", resultado);
+		// Test Impuesto
+		driver.findElement(By.id("btn_impuesto")).click();
+		TimeUnit.MILLISECONDS.sleep(sleepTimeMill);
+		resultado = driver.findElement(By.id("resultado")).getText();
+		assertEquals("Impuesto: 80000", resultado);
 
-	  driver.close();
+		driver.close();
 	}
 
 }
